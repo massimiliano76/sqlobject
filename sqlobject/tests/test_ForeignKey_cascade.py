0fde1fe77deb5769f12d05349a5da57e743a8ab4
Oleg Broytman <phd@phdru.name>
2019-09-29 19:00:52 +0300

    Fix a bug in cascade deletion/nullification

--- sqlobject/tests/test_ForeignKey_cascade.py
+++ sqlobject/tests/test_ForeignKey_cascade.py
@@ -91,9 +91,7 @@ def test3():
     raises(SQLObjectNotFound, emily.sync)
 
     assert message.recipient is None
-
-    # This looks like a bug; `message.sender` here should be None
-    # assert message.sender is None
+    assert message.sender is None
 
 
 class SOTestPerson4(SQLObject):
@@ -120,6 +118,24 @@ def test4():
     message.expire()
 
     john.sync()
+    raises(SQLObjectNotFound, message.sync)
 
-    # This is even a nastier bug; `message` was deleted from the DB
-    # message.sync()
+
+def test5():
+    setupClass([SOTestPerson4, SOTestMessageCascadeMixed])
+
+    john = SOTestPerson4(name='john')
+    emily = SOTestPerson4(name='emily')
+    message = SOTestMessageCascadeMixed(
+        sender=emily, recipient=john, body='test4'
+    )
+
+    john.destroySelf()
+    emily.expire()
+    message.expire()
+
+    emily.sync()
+    message.sync()
+
+    assert message.recipient is None
+    assert message.sender == emily
